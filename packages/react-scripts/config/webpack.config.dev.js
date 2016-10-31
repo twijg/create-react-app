344dd66aa8faff09ddcd987b70f1f25ef06833b6
Jelle de Jong <jelle@twijg-it.nl>
2016-10-31 11:29:04 +0100

    Changed order

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -138,7 +138,7 @@ module.exports = {
       // in development "style" loader enables hot editing of CSS.
       {
         test: /\.scss$/,
-        loader: 'style!css!sass!postcss'
+        loader: 'style!css!postcss!sass'
       },
       // JSON is not enabled by default in Webpack but both Node and Browserify
       // allow it implicitly so we also enable it.
